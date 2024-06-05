a0c687d97cd7bcb940547cfd5b01a845f0a86edb
Patchnida Hemwannanukul <patchnida2547@gmail.com>
2024-06-06 03:57:33 +0700

    create layout
    Create Header and MenuBar

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Header() {
+  return (
+    <>
+    <div className="w-full h-20 border-b-4 border-zinc-400 m-5 content-center">
+        <h1 className="text-5xl text-black text-left font-bold ">Header</h1>
+    </div>
+    </>
+  )
+}
+
+export default Header
