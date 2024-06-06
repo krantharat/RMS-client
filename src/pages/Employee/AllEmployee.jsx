1563cb8ca5795f8f355a649b94731212590ff168
Patchnida Hemwannanukul <patchnida2547@gmail.com>
2024-06-06 23:24:28 +0700

    update search and create button

--- src/pages/Employee/AllEmployee.jsx
+++ src/pages/Employee/AllEmployee.jsx
@@ -4,9 +4,20 @@ import Header from "../../components/header"
 function Employee() {
   return (
     <>
-    <div>
+    <div className="flex flex-col w-full">
       <Header title="Employee" />
-      <h1>This is All Employee</h1>
+      <div>
+        <form className="flex justify-end">
+          <input 
+            type="text" 
+            placeholder="Search employee by ID" 
+            className="border-0 rounded-3xl p-2" />
+          <button type="submit" className="w-20 bg-yellow-500 font-medium capitalize border-0 rounded-3xl ml-5 p-2">create</button>
+        </form>
+      </div>
+      <div>
+        <h1>Content part</h1>
+      </div>
     </div>
     </>
   )
